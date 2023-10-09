b144c4407aca28c386939d3c94709081bddb019d
jay-brdy <jaysirit@gmail.com>
2023-10-09 15:03:09 -0500

    code works perfectly but looking for ways to simplify. I already see a lot of repetition...

--- froyo.js
+++ froyo.js
@@ -4,6 +4,27 @@ const userInputString = prompt(
     "plain tart, strawberry, oreo"
   );
 
-// Split the string of flavors into an array of strings.
-const stringArray = userInputString.split(",");
 
+function trackFroyoFlavors(userInputString) {
+    // // Split the string of flavors into an array of strings.
+    const stringArray = userInputString.split(",");
+    // Empty object to track flavors
+    const flavorsObject = {};
+
+    // For loop to iterate through the stringArray, updates the obj and keep track of how many times each flavor appears
+    for (const flavor of stringArray) {
+        // Check if the flavor already exists in the object
+        if (flavorsObject[flavor]) {
+            // If flavor exists already, increment count by 1
+            flavorsObject[flavor]++;
+        } else {
+            // If flavor doesn't exist, init flavor with count of 1
+            flavorsObject[flavor] = 1;
+        }
+    }
+    return flavorsObject;
+}
+
+// Calls the trackFroyoFlavors function and stores the result in flavorsObject
+const flavorsObject = trackFroyoFlavors(userInputString);
+console.log(flavorsObject);
